01216cb9b65059e95bf800cf16098a13df70ec7d
Vinso <8vinso8@gmail.com>
2022-03-27 22:07:33 +1000

    Улучшен дизайн

--- homepage/forms.py
+++ homepage/forms.py
@@ -2,4 +2,6 @@ from django import forms
 
 
 class EmailForm(forms.Form):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(required=True, label='',widget=forms.EmailInput(attrs={'placeholder': 'Email',
+                                                                                    'style': 'width: 300px',
+                                                                                    'class': 'form-control'}))
