f2ff78c6de3f3e5d240a173d6e4b5775fb44742e
Vinso <8vinso8@gmail.com>
2022-03-28 18:55:52 +1000

    Дизайн поля для почты перенесен в html

--- homepage/forms.py
+++ homepage/forms.py
@@ -2,6 +2,5 @@ from django import forms
 
 
 class EmailForm(forms.Form):
-    email = forms.EmailField(required=True, label='',widget=forms.EmailInput(attrs={'placeholder': 'Email',
-                                                                                    'style': 'width: 300px',
-                                                                                    'class': 'form-control'}))
+    email = forms.EmailField(required=True, label='', widget=forms.EmailInput(attrs={'placeholder': 'Email',
+                                                                                     'class': 'form-control'}))
