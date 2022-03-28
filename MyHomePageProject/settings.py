0614b1d93da7cfdc6106bba49daa7ddcdaa91215
Vinso <8vinso8@gmail.com>
2022-03-29 02:00:39 +1000

    /

--- MyHomePageProject/settings.py
+++ MyHomePageProject/settings.py
@@ -74,8 +74,16 @@ WSGI_APPLICATION = 'MyHomePageProject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'MyDatabase'),
+    }
+}
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
-DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
